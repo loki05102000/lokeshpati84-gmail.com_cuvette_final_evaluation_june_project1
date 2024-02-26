eaf5ec12dd365d360be54c7bd6f36c05c7f22024
Lokesh Patidar <Lokeshpati84@gmail.com>
2024-02-26 17:02:17 +0530

    boilerplate nodejs setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "Success",
+    message: "All good!",
+  });
+});
+
+app.listen(process.env.PORT, () => {
+   console.log(`Server running on http://localhost:${process.env.PORT}`)
+  
+});
